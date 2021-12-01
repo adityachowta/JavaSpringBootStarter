81794ae5e3b26e47ad14ec5f2803bcfa3a881fa4
Chowta, Aditya <aditya.chowta@willistowerswatson.com>
2021-12-01 21:31:27 +0530

    new changes to Student name

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class StudentService {
     public List<Student> getStudent() {
         return List.of(
-                new Student(1L, "Aditya Vittal Chowta", 31, LocalDate.of(1990, Month.OCTOBER, 02), "aditya.chowta@gmail.com"));
+                new Student(1L, "Aditya Vittal Chowta", 31, LocalDate.of(1990, Month.OCTOBER, 02), "aditya.chowta@gmail.com"),
+                new Student(2L, "Tony Stark", 45, LocalDate.of(1990, Month.OCTOBER, 20), "tony.stark@gmail.com"));
     }
 }
